5bccb9e8d33480518f10c25aedbc5356f595b160
Javian <65435297+javianng@users.noreply.github.com>
2022-04-09 10:35:12 +0800

    tutorial4 submitted

--- Tutorial/Tutorial 4/tutorial4.py
+++ Tutorial/Tutorial 4/tutorial4.py
@@ -94,16 +94,64 @@ def accumulate_iter(combiner, null_value, term, a, next, b):
 # Question 6
 
 def make_point(x, y):
-    #Your code here
-    return 
+    return [x, y]
 
 def x_point(p):
-    #Your code here
-    return 
+    return p[0]
     
 def y_point(p):
-    #Your code here
-    return 
+    return p[1]
 
 #For running public test case, do not delete
 p1 = make_point(2, 3)
+
+# Question 7
+
+def make_segment(p1, p2):
+    return [p1,p2]
+    
+def start_segment(s):
+    return s[0]
+
+def end_segment(s):
+    #Your code here
+    return s[1]
+    
+#do not uncomment! this is for reference only.
+p1 = make_point(2, 3)
+p2 = make_point(5, 7)
+
+# Question 8
+
+'''Version 1
+
+def midpoint_segment(segment):
+    
+    x_coor = (segment[0][0] + segment[1][0]) / 2
+    y_coor = (segment[0][1] + segment[1][1]) / 2
+    
+    return make_point(x_coor, y_coor)
+    
+#for running public test case, do not delete!
+p1 = make_point(2, 3)
+p2 = make_point(5, 7)
+s = make_segment(p1, p2)
+
+print(midpoint_segment(s))'''
+
+
+'''Version 2'''
+
+def midpoint_segment(segment):
+    start_point = start_segment(segment)
+    end_point = end_segment(segment)
+
+    mid_x = 0.5 * (x_point(start_point) + x_point(end_point))
+    mid_y = 0.5 * (y_point(start_point) + y_point(end_point))
+
+    return make_point(mid_x, mid_y)
+
+#for running public test case, do not delete!
+p1 = make_point(2, 3)
+p2 = make_point(5, 7)
+s = make_segment(p1, p2)
