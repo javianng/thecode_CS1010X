f4e89218068deece0f3788afac3205c52475e410
Javian <65435297+javianng@users.noreply.github.com>
2022-04-19 04:32:10 +0800

    commit

--- Side_Quest/sidequest5.3/sidequest05.3-template.py
+++ Side_Quest/sidequest5.3/sidequest05.3-template.py
@@ -12,8 +12,24 @@ from hi_graph_connect_ends import *
 # Task 1 #
 ##########
 
+def yj_dragonize(num_points , curve):
+    if num_points == 0:
+        return curve
+    else:
+        c = yj_dragonize(num_points -1, curve)
+        return put_in_standard_position(connect_ends(rotate(-pi/2)(c), c))
+
+
 def dragonize(order, curve):
-    "your answer here"
+    if order == 0:
+        return curve
+    else:
+        c = dragonize(order-1, curve)
+        return put_in_standard_position(connect_ends(revert(c), rotate(pi/2)(c)))
+
+def revert(curve):
+    return lambda t: curve(1-t)
 
 # test:
 # draw_connected_scaled(4096, dragonize(12, unit_line))
+# draw_connected_scaled(10, yj_dragonize(2 , unit_line))
