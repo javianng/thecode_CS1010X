0ee6aab5fcfcdf31aa49d66f76bf37d07f8bb774
Javian <65435297+javianng@users.noreply.github.com>
2022-04-27 12:03:47 +0800

    comit

--- Lecture/lecture12.py
+++ Lecture/lecture12.py
@@ -48,3 +48,98 @@ class Fox(Mammal):
 
 # Implement additional methods to your Dog, Cat and Fox subclasses to modify what they say.
 
+class Dog(Mammal):
+    def __init__(self):
+        self.name = 'Dog'
+        super().__init__('Dog')
+    def say(self):
+        return "Woof"
+        
+# Cat definition should go here
+
+class Cat(Mammal):
+    def __init__(self):
+        self.name = 'Cat'
+        super().__init__('Cat')
+    def say(self):
+        return "Meow"
+        
+# Fox definition should go here
+
+class Fox(Mammal):
+    def __init__(self):
+        self.name = 'Fox'
+        super().__init__('Fox')
+    def say(self):
+        return "Ring-ding-ding-ding-dingeringeding"
+        
+# print(Dog().say())
+# print(Mammal("Dog").say())
+
+######################
+# Question 4: Artist #
+######################
+
+class Artist(object):
+    def __init__(self, name, dob):
+        self.name = name
+        self.dob = dob
+        
+    def get_name(self):
+        return self.name
+        
+    def get_dob(self):
+        return self.dob
+        
+# Used for public test cases.
+# You DO NOT have to include this in your submission.
+jt = Artist("Justin Timberlake", (1981, 1, 31))
+
+# print(jt.get_name())
+# print(jt.get_dob())
+# print(Artist("JC Chasez", (1976, 8, 8)).get_name())
+
+##########################
+# Question 5: Artist Age #
+##########################
+
+def get_date_today():
+    return (2013, 10, 30)
+
+def leap_year_identifier(year):
+    if (year % 400 == 0) and (year % 100 == 0):
+        return True
+    elif (year % 4 ==0) and (year % 100 != 0):
+        return True
+    else:
+        return False
+    
+class Artist(object):
+    def __init__(self, name, dob):
+        self.name = name
+        self.dob = dob
+        
+    def age(self):
+        if self.dob[1] == 2 and self.dob[2] == 29: 
+            if leap_year_identifier(get_date_today()[0]) == False:
+                self.dob[1], self.dob[2] = 3, 1
+        
+        number = get_date_today()[0] - self.dob[0]
+        
+        if self.dob[1] > get_date_today()[1]:
+            return number - 1
+        elif self.dob[1] == get_date_today()[1]:
+            if self.dob[2] >= get_date_today()[2]:
+                return number - 1
+            else:
+                return number
+        else:
+            return number
+        
+
+# Used for public test cases.
+# You DO NOT have to include this in your submission
+jt = Artist("Justin Timberlake", (1981, 1, 31))
+
+# print(jt.age())
+# print(Artist("Hayley Williams", (1988, 12, 27)).age())
