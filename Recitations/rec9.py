aab2f97d178624656c69a59f76a3a59482585290
Javian <65435297+javianng@users.noreply.github.com>
2022-04-30 20:50:08 +0800

    commit after lecture

--- Recitations/rec9.py
+++ Recitations/rec9.py
@@ -1,5 +1,8 @@
 # Question 1
 
+from xxlimited import foo
+
+
 class Food(object):
     def __init__(self, name, nutrition, good_until):
         self.name = name
@@ -24,9 +27,56 @@ class AgedFood(Food):
         self.good_after = good_after
         
     def sniff(self):
-        if self.age > self.good_after:
-            return True
-        else:
-            return False
+        return self.age > self.good_after
         
-    def eat(self)
+    def eat(self): # ??????????????????????????????
+        if self.sniff():
+            return super().eat(self.age)
+        else:
+            return 0
+        
+# Question 3
+
+class VendingMachine(object):
+    def __init__(self, name, nutrition, good_until):
+        self.name = name
+        self.nutrition = nutrition
+        self.good_until = good_until
+        self.age = 0
+        
+    def sit_there(self, time):
+        self.age += time/2
+        
+    def sell_food(self):
+        food = Food(self.name, self.nutrition, self.good_until)
+        food.age = self.age
+        return food
+    
+# Question 4
+
+def mapn (fn , lsts):
+# fn is the function that you would apply to the lsts
+# lsts is the list of lists that would given as input to the function fn
+    pass
+
+# Question 5
+
+class VendingMachine(object):
+    def __init__(self, type_food, name, nutrition, good_until, *good_after):
+        self.name = name
+        self.nutrition = nutrition
+        self.good_until = good_until
+        self.age = 0
+        self.type_food = type_food
+        
+    def sit_there(self, time):
+        self.age += time/2
+        
+    def sell_food(self):
+        if ...
+        food = self.type_food(self.name, self.nutrition, self.good_until)
+        food.age = self.age
+        return food
+    
+    def sell_agedFood(self):
+        ...
