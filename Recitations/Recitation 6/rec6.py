331f75264985400709ff4b8b32f6b70685225548
Javian <65435297+javianng@users.noreply.github.com>
2022-05-14 13:03:45 +0800

    commit

--- Recitations/Recitation 6/rec6.py
+++ Recitations/Recitation 6/rec6.py
@@ -39,3 +39,73 @@ singleton = ['blah ']
 # print ( singleton ) # ['blah ']
 
 # Question 2
+
+def bubble(lst):
+    for j in range(len(lst)-1):
+        for i in range(len(lst)-1-j):
+            if lst[i] > lst[i+1]:
+                lst[i], lst[i+1] = lst[i+1], lst[i]
+
+# Time Complexity: O(n**2)
+# Space Complexity: O(1) # 2 extra variables required
+# Between bubble with "for i in range(len(lst)-1-j):"" and bubble with "for i in range(len(lst)-1):"
+# the first version is faster but their time complexity is the same.
+
+# Question 3
+
+def selection(lst):
+    for i in range in range(len(lst)-1):
+        for j in range(i+1,len(lst)):
+            if lst[i] > lst[j]:
+                lst[i], lst[j] = lst[j], lst[i]
+
+# Time Complexity: O(n**2)
+# Space Complexity: O(1) # 2 extra variables required
+
+# Question 4
+
+students = [
+('tiffany ', 'A', 15),
+('jane ', 'B', 10),
+('ben ', 'C', 8),
+('simon ', 'A', 21),
+('john ', 'A', 15),
+('jimmy ', 'F', 1),
+('charles ', 'C', 9),
+('freddy ', 'D', 4),
+('dave ', 'B', 12 )]
+
+# a
+
+# >>> students.sort (key=lambda x:x[0], reverse=True)
+
+# b
+
+# >>> students.sort (key=lambda x:x[0])
+# >>> students.sort (key=lambda x:x[1])
+
+# c
+
+# Method 1
+
+# >>> a = tuple(filter(lambda x: len(x[0]) < 6, students))
+# >>> tuple(map(lambda x:x[0], a))
+
+# Method 2
+
+# >>> a = tuple(map(lambda x: x[0], students))
+# >>> tuple(filter(lambda x: len(x) < 6, a))
+
+# d
+
+def count(students): 
+    grades = tuple(map(lambda x: x[1], students)) # returns a tuple of just the grades
+    def count_freq(grades): # solving via recursion
+        if grades == (): 
+            return () 
+        else: 
+            first = grades[0] # obtaining the first grade
+            count = len(tuple(filter(lambda x: x == first, grades))) # filter to get only the desired grade and count them
+            rest = tuple(filter(lambda x: x != first, grades)) # get remaining grades tuple
+            return ((first, count),) + count_freq(rest) # recursively add grade and its count
+    return count_freq(grades)
