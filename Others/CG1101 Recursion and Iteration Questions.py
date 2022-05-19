c8d74ccb843b41cf0a01028713bdd11b8e94a257
Javian <65435297+javianng@users.noreply.github.com>
2022-05-19 17:09:37 +0800

    commit

--- Others/CG1101 Recursion and Iteration Questions.py
+++ Others/CG1101 Recursion and Iteration Questions.py
@@ -2,17 +2,32 @@
 # Question 1: Legendre's Conjecture #
 #####################################
 
-def legendre(n):
-    
-    pass
-
 def prime_number_checker(number):
-    if number > 1:
-    # check for factors
-        for i in range(2,number):
-            if (number % i) == 0:
-                return False
-            else:
-                return True
+    for n in range(2,int(number**0.5)+1):
+        if number%n==0:
+            return False
+    return True
+
+result = False
+
+def legendre(n):
+    if n == 0:
+        global result 
+        result = True
+        return result
     else:
-        return False
+        inter_result = False
+        for number in range(n**2, (n+1)**2):
+            if prime_number_checker(number) == True:
+                inter_result = True
+                break
+        if inter_result == True:
+            return legendre(n-1)
+
+# print(legendre(9))
+# print(prime_number_checker(13))
+
+###############################################
+# Question 2: Legendre's Conjecture - Part 2! #
+###############################################
+
