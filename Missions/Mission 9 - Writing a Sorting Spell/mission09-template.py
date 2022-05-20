790d5247f34e6d27da83c6c6aaa58cdee6199a09
Javian <65435297+javianng@users.noreply.github.com>
2022-05-20 20:01:50 +0800

    commit

--- Missions/Mission 9 - Writing a Sorting Spell/mission09-template.py
+++ Missions/Mission 9 - Writing a Sorting Spell/mission09-template.py
@@ -43,20 +43,23 @@ def accumulate(fn, initial, seq):
 def search(x, seq):
     """ Takes in a value x and a sorted sequence seq, and returns the
     position that x should go to such that the sequence remains sorted """
-    return
+    for i, elem in enumerate(seq):
+        if x <= elem:
+            return i
+    return len(seq)
 
-print("## Q1a ##")
-print(search(-5, (1, 5, 10)))
+# print("## Q1a ##")
+# print(search(-5, (1, 5, 10)))
 # => 0
-print(search(3, (1, 5, 10)))
+# print(search(3, (1, 5, 10)))
 # => 1
-print(search(7, [1, 5, 10]))
+# print(search(7, [1, 5, 10]))
 # => 2
-print(search(5, (1, 5, 10)))
+# print(search(5, (1, 5, 10)))
 # => 1
-print(search(42, [1, 5, 10]))
+# print(search(42, [1, 5, 10]))
 # => 3
-print(search(42, (-5, 1, 3, 5, 7, 10)))
+# print(search(42, (-5, 1, 3, 5, 7, 10)))
 # => 6
 
 ###########
@@ -67,20 +70,30 @@ def binary_search(x, seq):
     """ Takes in a value x and a sorted sequence seq, and returns the
     position that x should go to such that the sequence remains sorted.
     Uses O(lg n) time complexity algorithm."""
-    return
+    def searcher(low, high):       
+        if low > high:
+            return low
+        mid = (low + high) // 2
+        if x < seq[mid]:
+            return searcher(low, mid-1)
+        elif x > seq[mid]:
+            return searcher(mid+1, high)
+        elif x == seq[mid]:
+            return mid
+    return searcher(0, len(seq)-1)
 
-print("## Q1b ##")
-print(binary_search(-5, (1, 5, 10)))
+# print("## Q1b ##")
+# print(binary_search(-5, (1, 5, 10)))
 # => 0
-print(binary_search(3, (1, 5, 10)))
+# print(binary_search(3, (1, 5, 10)))
 # => 1
-print(binary_search(7, [1, 5, 10]))
+# print(binary_search(7, [1, 5, 10]))
 # => 2
-print(binary_search(5, (1, 5, 10)))
+# print(binary_search(5, (1, 5, 10)))
 # => 1
-print(binary_search(42, [1, 5, 10]))
+# print(binary_search(42, [1, 5, 10]))
 # => 3
-print(binary_search(42, (-5, 1, 3, 5, 7, 10)))
+# print(binary_search(42, (-5, 1, 3, 5, 7, 10)))
 # => 6
 
 ###########
@@ -90,14 +103,16 @@ print(binary_search(42, (-5, 1, 3, 5, 7, 10)))
 def insert_list(x, lst):
     """ Inserts element x into list lst such that x is less than or equal
         to the next element and returns the resulting list."""
-    return
+    y = search(x, lst) # position
+    lst.insert(y, x)
+    return lst
 
-print("## Q2a ##")
-print(insert_list(2, [1, 5, 9]))
+# print("## Q2a ##")
+# print(insert_list(2, [1, 5, 9]))
 #=> [1, 2, 5, 9]
-print(insert_list(10, [1, 5, 9]))
+# print(insert_list(10, [1, 5, 9]))
 #=> [1, 5, 9, 10]
-print(insert_list(5, [2, 6, 8]))
+# print(insert_list(5, [2, 6, 8]))
 #=> [2, 5, 6, 8]
 
 ###########
@@ -107,14 +122,26 @@ print(insert_list(5, [2, 6, 8]))
 def insert_tup(x, tup):
     """ Inserts element x into tuple tup such that x is less than or equal
         to the next element and returns the resulting tuple."""
-    return
+    y = search(x, tup) # position
+    new_tup = ()
+    if y == len(tup):
+        tup += (x,)
+        return tup
+    else:
+        for i in range(0, len(tup)):
+            if i == y:
+                new_tup += (x,)
+                new_tup += (tup[i],)
+            else:
+                new_tup += (tup[i],)
+        return new_tup
 
-print("## Q2b ##")
-print(insert_tup(2, (1, 5, 9)))
+# print("## Q2b ##")
+# print(insert_tup(2, (1, 5, 9)))
 #=> (1, 2, 5, 9)
-print(insert_tup(10, (1, 5, 9)))
+# print(insert_tup(10, (1, 5, 9)))
 #=> (1, 5, 9, 10)
-print(insert_tup(5, (2, 6, 8)))
+# print(insert_tup(5, (2, 6, 8)))
 #=> (2, 5, 6, 8)
 
 ###########
@@ -126,16 +153,16 @@ output_tup = insert_tup(7, tup)
 lst = [5, 4, 10]
 output_lst = insert_list(7, lst)
 
-print("## Q2c ##")
-print(tup is output_tup)
-#=> Output:
-print(tup == output_tup)
-#=> Output:
+# print("## Q2c ##")
+# print(tup is output_tup)
+#=> Output: False
+# print(tup == output_tup)
+#=> Output: False
 
-print(lst is output_lst)
-#=> Output:
-print(lst == output_lst)
-#=> Output:
+# print(lst is output_lst)
+#=> Output: True
+# print(lst == output_lst)
+#=> Output: True
 #=> Explain the outputs:
 #=>
 #=>
@@ -146,17 +173,25 @@ print(lst == output_lst)
 ###########
 
 def sort_list(lst):
-    """ Sorts list lst in ascending order."""
-    return
+    """ Sorts list lst in an ascending order."""
+    sorted_list = []
+    if lst == []:
+        return lst
+    else:
+        sorted_list.append(lst[0])
+        del lst[0]
+        for i in lst:
+            insert_list(i, sorted_list)
+        return sorted_list
 
-print("## Q3a ##")
-print(sort_list([9, 6, 2, 4, 5]))
+# print("## Q3a ##")
+# print(sort_list([9, 6, 2, 4, 5]))
 #=> [2, 4, 5, 6, 9]
-print(sort_list([42, 7, 6, -42, 0]))
+# print(sort_list([42, 7, 6, -42, 0]))
 #=> [-42, 0, 6, 7, 42]
-print(sort_list(["soda", "cola", "sprite", "root beer", "apple cider"]))
-#=> ['apple cider', 'cola', 'root beer', 'soda', 'sprite']
-print(sort_list(["turtle", "penguin", "dog", "cat", "ant eater", "butterfly"]))
+# print(sort_list(["soda", "cola", "sprite", "root beer", "apple cider"]))
+# => ['apple cider', 'cola', 'root beer', 'soda', 'sprite']
+# print(sort_list(["turtle", "penguin", "dog", "cat", "ant eater", "butterfly"]))
 #=> ['ant eater', 'butterfly', 'cat', 'dog', 'penguin', 'turtle']
 
 ###########
@@ -171,16 +206,16 @@ print(sort_list(["turtle", "penguin", "dog", "cat", "ant eater", "butterfly"]))
 
 def sort_tup(tup):
     """ Sorts tuple tup in an ascending order."""
-    return
+    return accumulate(insert_tup, (), tup)
 
-print("## Q3c ##")
-print(sort_tup((9, 6, 2, 4, 5)))
+# print("## Q3c ##")
+# print(sort_tup((9, 6, 2, 4, 5)))
 #=> (2, 4, 5, 6, 9)
-print(sort_tup((42, 7, 6, -42, 0)))
+# print(sort_tup((42, 7, 6, -42, 0)))
 #=> (-42, 0, 6, 7, 42)
-print(sort_tup(("soda", "cola", "sprite", "root beer", "apple cider")))
-#=> ('apple cider', 'cola', 'root beer', 'soda', 'sprite')
-print(sort_tup(("turtle", "penguin", "dog", "cat", "ant eater", "butterfly")))
+# print(sort_tup(("soda", "cola", "sprite", "root beer", "apple cider")))
+# => ('apple cider', 'cola', 'root beer', 'soda', 'sprite')
+# print(sort_tup(("turtle", "penguin", "dog", "cat", "ant eater", "butterfly")))
 #=> ('ant eater', 'butterfly', 'cat', 'dog', 'penguin', 'turtle')
 
 ###########
@@ -200,24 +235,35 @@ def insert_animate(block_pos, shelf, high):
     NOTE: To actually see the animation, please uncomment the testing function
     below.
     """
+    low = 0
+    while low < high: # insert_animate(3, s, 3) # 4
+        block = shelf.pop(block_pos) # (1, 2, 6, __ 8, 3, 9)
+        if block.size > shelf[low].size:
+            low += 1
+            shelf.insert(low, block)
+            block_pos = low
+        elif block.size <= shelf[low].size:
+            shelf.insert(low, block)
+            break
+            
     # optional to return shelf but we do this for debugging
     return shelf
 
 # Test cases for insert_animate
 
-def test_insert_animate():
-    shelf.clear_window()
-    s = shelf.init_shelf((2, 6, 1, 4, 8, 3, 9))
-    print("## Q4a ##")
-    print(insert_animate(0, s, 0))
+# def test_insert_animate():
+#     shelf.clear_window()
+#     s = shelf.init_shelf((2, 6, 1, 4, 8, 3, 9))
+    # print("## Q4a ##")
+    # print(insert_animate(0, s, 0))
     # => [Block size: 2, Block size: 6, Block size: 1, Block size: 4, Block size: 8, Block size: 3, Block size: 9]
-    print(insert_animate(1, s, 1))
+    # print(insert_animate(1, s, 1))
     # => [Block size: 2, Block size: 6, Block size: 1, Block size: 4, Block size: 8, Block size: 3, Block size: 9]
-    print(insert_animate(2, s, 2))
+    # print(insert_animate(2, s, 2))
     # => [Block size: 1, Block size: 2, Block size: 6, Block size: 4, Block size: 8, Block size: 3, Block size: 9]
-    print(insert_animate(3, s, 3))
+    # print(insert_animate(3, s, 3))
     # => [Block size: 1, Block size: 2, Block size: 4, Block size: 6, Block size: 8, Block size: 3, Block size: 9]
-    print(insert_animate(6, s, 6))
+    # print(insert_animate(6, s, 6))
     # => [Block size: 1, Block size: 2, Block size: 4, Block size: 6, Block size: 8, Block size: 3, Block size: 9]
 
 # Uncomment function call to test insert_animate()
@@ -236,28 +282,31 @@ def sort_me_animate(shelf):
     below.
 
     """
+    for i in range(len(shelf)):
+        insert_animate(i, shelf, i)
+    
     # optional to return shelf but we do this for debugging
     return shelf
 
 # Test cases for sort_me_animate
 
-def test_sort_me_animate():
-    shelf.clear_window()
-    s = shelf.init_shelf((5,2,6,9,1,4,8,3))
-    print("## Q4b ##")
-    print(sort_me_animate(s))
+# def test_sort_me_animate():
+#     shelf.clear_window()
+#     s = shelf.init_shelf((5,2,6,9,1,4,8,3))
+    # print("## Q4b ##")
+    # print(sort_me_animate(s))
     # => [Block size: 1, Block size: 2, Block size: 3, Block size: 4, Block size: 5, Block size: 6, Block size: 8, Block size: 9]
-    shelf.clear_window()
-    s = shelf.init_shelf((4, 8, 2, 9, 3, 1, 2, 3, 4, 10, 7, 5, 6))
-    print(sort_me_animate(s))
+    # shelf.clear_window()
+    # s = shelf.init_shelf((4, 8, 2, 9, 3, 1, 2, 3, 4, 10, 7, 5, 6))
+    # print(sort_me_animate(s))
     # => [Block size: 1, Block size: 2, Block size: 2, Block size: 3, Block size: 3, Block size: 4, Block size: 4, Block size: 5, Block size: 6, Block size: 7, Block size: 8, Block size: 9, Block size: 10]
 
 # Test case to catch mutation while sorting
 
-def test_sort_me_with_duplicates():
-    shelf.clear_window()
-    s = shelf.init_shelf((1,3,4,1,3,2))
-    print(sort_me_animate(s))
+# def test_sort_me_with_duplicates():
+#     shelf.clear_window()
+#     s = shelf.init_shelf((1,3,4,1,3,2))
+#     print(sort_me_animate(s))
     # => [Block size: 1, Block size: 1, Block size: 2, Block size: 3, Block size: 3, Block size: 4]
 
 # Uncomment function call to test sort_me_animate()
