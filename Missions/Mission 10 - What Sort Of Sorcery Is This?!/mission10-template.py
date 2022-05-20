d40a8d1d480fab4f2e5230c334a01689097754bc
Javian <65435297+javianng@users.noreply.github.com>
2022-05-20 23:37:34 +0800

    commit

--- Missions/Mission 10 - What Sort Of Sorcery Is This?!/mission10-template.py
+++ Missions/Mission 10 - What Sort Of Sorcery Is This?!/mission10-template.py
@@ -6,12 +6,14 @@
 # Note that written answers are commented out to allow us to run your
 # code easily while grading your problem set.
 
-
 ####################
 # Helper functions #
 # - print_tree     #
 # - accumulate     #
 ####################
+
+from distutils.command.build import build
+
 
 def print_tree(tree, print_output=True):
     """
@@ -66,69 +68,50 @@ def accumulate(fn, initial, seq):
     else:
         return fn(seq[0], accumulate(fn, initial, seq[1:]))
 
-
 ###########
 # Task 1a #
 ###########
 
 def build_tree(entry, left, right):
-    # Your code here
-    return
-
+    return [entry, left, right]
 
 ###########
 # Task 1b #
 ###########
 
 def entry(tree):
-    # Your code here
-    return
+    return tree[0]
 
 def left_branch(tree):
-    # Your code here
-    return
+    return tree[1]
 
 def right_branch(tree):
-    # Your code here
-    return
-
+    return tree[2]
 
 ###########
 # Task 1c #
 ###########
 
 def make_empty_tree():
-    # Your code here
-    return
-
+    build_tree(None, None, None)
 
 ###########
 # Task 1d #
 ###########
 
 def is_empty_tree(tree):
-    # Your code here
-    return
+    return tree == None
 
-t1 = build_tree(2, build_tree(1, make_empty_tree(),
-                                 make_empty_tree()),
-                   build_tree(3, make_empty_tree(),
-                                 make_empty_tree()))
-print_tree(t1)
+t1 = build_tree(2, build_tree(1, make_empty_tree(), make_empty_tree()), build_tree(3, make_empty_tree(), make_empty_tree()))
+# print_tree(t1)
 #=> 2
 #=>1 3
 
-t2 = build_tree(5, build_tree(2, build_tree(1, make_empty_tree(),
-                                               make_empty_tree()),
-                                 make_empty_tree()),
-                   build_tree(7, make_empty_tree(),
-                                 build_tree(10, make_empty_tree(),
-                                                make_empty_tree())))
-print_tree(t2)
+t2 = build_tree(5, build_tree(2, build_tree(1, make_empty_tree(), make_empty_tree()), make_empty_tree()), build_tree(7, make_empty_tree(), build_tree(10, make_empty_tree(), make_empty_tree())))
+# print_tree(t2)
 #=>   5
 #=> 2   7
 #=>1     10
-
 
 ###########
 # Task 2a #
@@ -140,34 +123,36 @@ def insert_tree(x, tree):
     - x <= entry -> return new tree with x inserted into left sub tree
     - otherwise -> return new tree with x inserted into right sub tree
     """
-    # Your code here
-    return
+    if is_empty_tree(tree) == True:
+        return build_tree(x, make_empty_tree(), make_empty_tree())
+    elif x <= entry(tree):
+        return build_tree(entry(tree), insert_tree(x, left_branch(tree)), right_branch(tree))
+    else:
+        return build_tree(entry(tree), left_branch(tree), insert_tree(x, right_branch(tree)))
 
 t1 = insert_tree(5, t1)
-print_tree(t1)
+# print_tree(t1)
 #=> 2           insert_tree(5, t1)        2
 #=>1 3               ===>               1   3
 #=>                                          5
 
 t2 = insert_tree(6, t2)
-print_tree(t2)
+# print_tree(t2)
 #=>   5         insert_tree(6, t2)        5
 #=> 2   7            ===>               2   7
 #=>1     10                            1   6 10
 
 t2 = insert_tree(3, t2)
-print_tree(t2)
+# print_tree(t2)
 #=>   5         insert_tree(3, t2)        5
 #=> 2   7            ===>               2   7
 #=>1   6 10                            1 3 6 10
-
 
 ###########
 # Task 2b #
 ###########
 
-# Time complexity of insert_tree:
-
+# Time complexity of insert_tree: O(N)
 
 ###########
 # Task 2c #
@@ -175,34 +160,38 @@ print_tree(t2)
 
 def contains(x, tree):
     """ Returns true if x is in binary tree, otherwise return false """
-    # Your code here
-    return
+    if is_empty_tree(tree):
+        return False
+    elif entry(tree) == x:
+        return True
+    elif x < entry(tree):
+        return contains(x, left_branch(tree))
+    else:
+        return contains(x, right_branch(tree))
 
-print(contains(1, t1))
+# print(contains(1, t1))
 #=> True
 
-print(contains(5, t1))
+# print(contains(5, t1))
 #=> True
 
-print(contains(42, t1))
+# print(contains(42, t1))
 #=> False
 
-print(contains(10, t2))
+# print(contains(10, t2))
 #=> True
 
-print(contains(6, t2))
+# print(contains(6, t2))
 #=> True
 
-print(contains(11, t2))
+# print(contains(11, t2))
 #=> False
-
 
 ###########
 # Task 2d #
 ###########
 
-# Time complexity of contains:
-
+# Time complexity of contains: O(N)
 
 ###########
 # Task 2e #
@@ -211,30 +200,36 @@ print(contains(11, t2))
 def flatten(tree):
     """ flattens tree with the following rule:
         visit left branch, visit entry then visit right branch """
-    # Your code here
-    return
+    # Traversing binary tree
+    if is_empty_tree(tree):
+        return []
+    else:
+        result = flatten(left_branch(tree))
+        result.extend([entry(tree)])
+        result.extend(flatten(right_branch(tree)))
+    return result
 
-print(flatten(t1))
+# print(flatten(t1))
 #=> [1, 2, 3, 5]
 
-print(flatten(t2))
+# print(flatten(t2))
 #=> [1, 2, 3, 5, 6, 7, 10]
-
 
 ###########
 # Task 2f #
 ###########
 
-# Time complexity of flatten:
-
+# Time complexity of flatten: O(N^2)
 
 ###########
 # Task 3a #
 ###########
 
 def sort_it(lst):
-    # Your code here
-    return
+    sorted_tree = make_empty_tree()
+    for element in lst:
+        sorted_tree = insert_tree(element, sorted_tree)
+    return flatten(sorted_tree)
 
 print(sort_it([5, 3, 2, 1, 4, 6, 7, 9]))
 #=> [1, 2, 3, 4, 5, 6, 7, 9]
@@ -242,9 +237,8 @@ print(sort_it([5, 3, 2, 1, 4, 6, 7, 9]))
 print(sort_it([5, 3, 2, 1, 4, -1, 6, 0, 7, 9]))
 #=> [-1, 0, 1, 2, 3, 4, 5, 6, 7, 9]
 
-
 ###########
 # Task 3b #
 ###########
 
-# Time complexity of sort_it:
+# Time complexity of sort_it: O(N^4)
