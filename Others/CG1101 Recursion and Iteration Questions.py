4a2fe57e160922f0301dfd0c12b02ce758e0e442
Javian <65435297+javianng@users.noreply.github.com>
2022-05-20 10:31:54 +0800

    commit

--- Others/CG1101 Recursion and Iteration Questions.py
+++ Others/CG1101 Recursion and Iteration Questions.py
@@ -54,37 +54,33 @@ def legendre_n(n):
 
 # every even integer greater than 2 can be written as the sum of two primes.
 
-# def eratosthenes(n):
-#     primes = list (range(2, n+1))
-#     for i in primes:
-#         j=2
-#         while i*j<= primes[-1]:
-#             if i*j in primes:
-#                 primes.remove(i*j)
-#             j=j+1
-#     return primes
+def goldbach(n):
+    def list_of_odd_primes(n):
+        primes = list(range(2, n+1))
+        for i in primes:
+            j=2
+            while i*j<= primes[-1]:
+                if i*j in primes:
+                    primes.remove(i*j)
+                j=j+1
+        primes.remove(2)
+        return primes
+    x, y = 0, 0
+    result = 0
+    if n % 2 == 0: # check for even n input
+        prime = list_of_odd_primes(n)
+        while result != n:
+            for i in range(len(prime)):
+                if result == n: 
+                    return True
+                x = prime[i]
+                for j in range(len(prime)):
+                    y = prime[j]
+                    result = x + y
+                    if result == n: break
+    return False
 
-# def odd_primes(N):
-#     oddprimes = eratosthenes(N)
-#     oddprimes.remove(2)
-#     return(oddprimes)
-
-# def goldbach(N):
-#     x, y = 0, 0
-#     result = 0
-#     if N % 2 == 0:
-#         prime = odd_primes(N)
-#         while result != N:
-#             for i in range(len(prime)):
-#                 if result == N: break 
-#                 x = prime[i]
-#                 for j in range(len(prime)):
-#                     y = prime[j]
-#                     result = x + y
-#                     if result == N: break 
-#     return x, y 
-
-# print(goldbach(4))
+print(goldbach(1000))
 
 ################################
 # Question 4: Maclaurin Series #
